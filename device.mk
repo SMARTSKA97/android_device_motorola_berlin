056fe3370b61c78f4c9ee231948242e1bed0cb8d
SKA97 <karmakarsubhrajit680@gmail.com>
2025-05-20 10:39:23 +0530

    berlin: update shipping API level to 35

--- device.mk
+++ device.mk
@@ -15,7 +15,7 @@ PRODUCT_AAPT_CONFIG := normal
 PRODUCT_AAPT_PREF_CONFIG := 400dpi
 PRODUCT_AAPT_PREBUILT_DPI := xxxhdpi xxhdpi xhdpi hdpi
 
-PRODUCT_SHIPPING_API_LEVEL := 34
+PRODUCT_SHIPPING_API_LEVEL := 35
 
 # Inherit from motorola sm7325-common
 $(call inherit-product, device/motorola/sm7325-common/common.mk)
